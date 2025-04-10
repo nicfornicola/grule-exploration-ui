eaae15384338eb65dcd5d002940bc0fa7fbc31f0
Nic Fornicola <nicfornicola@gmail.com>
2025-04-10 11:19:24 -0500

    mock data from lambda

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import mockResponse from '../../mockResponse.json'
+import mockResponse from './mockResponse.json'
+import bronzeMedal from './assets/bronze_medal.svg';
+import silverMedal from './assets/silver_medal.svg';
+import goldMedal from './assets/gold_medal.svg';
 
+const medals = [goldMedal, silverMedal, bronzeMedal]
 
 function App() {
-    const [response, setResponse] = useState(mockResponse);
+    const [response, setResponse] = useState([]);
     console.log(response)
     const [themes, setThemes] = useState({
         adventure: true,
@@ -26,6 +30,23 @@ function App() {
             [theme]: !prevThemes[theme],
         }));
     };
+
+    const handleSubmit = async () => {
+        try {
+            const response = await fetch('https://v8mqj3heg5.execute-api.us-east-2.amazonaws.com/dev/songs', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(themes),
+            });
+            const data = await response.json();
+            setResponse(data['body-json']);
+            console.log('Response from AWS:', data['body-json']);
+        } catch (error) {
+            console.error('Error submitting themes:', error);
+        }
+    };
     
     return (
         <div className='appContainer'>
@@ -41,25 +62,23 @@ function App() {
                 ))}
                 
             </div>
-
-            <button className='submitButton'>
+            
+            <button className='submitButton' onClick={handleSubmit}>
                 Submit
             </button>
-
             <div className='recommendedSongsContainer'>
-                {response.recommendedSongs.map(songObject => {
+                {response.map((songObject, index) => {
                     return (
-                        <div className='recommendedSong'>
-                            <h3 className='songTitle'>{songObject.title} - {songObject.artist}</h3>
+                        <div key={songObject.RuleID} className='recommendedSong'>
+                            <h3 className='songTitle'>{songObject.Title} - {songObject.Artist}</h3>
+                            {index < 3 && <img className='medalIcon' src={medals[index]}/>}
                             <ul className='themesList'>
-                                {Object.entries(songObject.themes).map(([theme, desc]) => {
-                                    return <>
-                                            {desc !== "" && 
-                                                <li className='themesListItem'>
+                                {Object.entries(songObject.Themes).map(([theme, desc]) => {
+                                    return desc !== "" && (
+                                                <li key={songObject.RuleID + theme} className='themesListItem'>
                                                     <b>{theme}</b>: {desc}
-                                                </li>
-                                            }
-                                        </>
+                                                </li> 
+                                            ) 
                                 })}
                             </ul>
 
